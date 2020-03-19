d5f398fa4e05a5a05f62322bdb3e957eda34705c
CortesBeringola <rafael.cortes.beringola@gmail.com>
2020-03-19 14:13:49 -0500

    initial commit

---
+++ 1st/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/rafaelcortesberingola/Documents/GitHub/Django/1st/1st/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
